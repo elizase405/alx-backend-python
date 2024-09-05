ba3977ea1c3de96d11c601391950427ffb412816
Elizabeth <elizase405@gmail.com>
2024-09-05 09:30:37 -0700

    feat: task 1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """1 function: add(a, b) - a+b """
 
+
 def add(a: float, b: float) -> float:
-	"""returns the sum of a + b"""
-	return a + b
+    """returns the sum of a + b"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""1 function: concat(str1, str2) - a+b """
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate str1 and str2 into 1"""
+    return str1 + str2
