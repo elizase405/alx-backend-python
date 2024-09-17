cb71efe54095f2116c320f5fd8c2c2815e244d67
Elizabeth <elizase@gmail.com>
2024-09-17 19:50:06 +0000

    misc: Add doc and fix oycodestyle error

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,7 +2,9 @@
 """ module with 1 function: safe_first_element  """
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ if list return lst[0] else return None """
     if lst:
         return lst[0]
     else:
