a721e321b454906fd5e55bfc3d066e103c267faf
Elizabeth <elizase405@gmail.com>
2025-01-20 01:26:46 -0800

    fix: annotate return value

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,10 +4,11 @@ function task_wait_random(3) - returns asyncio Task.
 """
 
 import asyncio
+from asyncio import Task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Task:
     """
     returns a asyncio.Task
     """
